33605e888f215c4b12b80f23471a4c9620fa44a0
Rafa <rafa@cabrerizos.fc>
2026-04-22 13:03:10 +0200

    Fix router basename

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/cabrerizos-fc/">
         <App />
       </BrowserRouter>
     </AuthProvider>
