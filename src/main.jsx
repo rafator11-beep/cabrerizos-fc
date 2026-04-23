517d6c2d32392f44ced2ecc5990251d819876708
rafator11-beep <rafator11@gmail.com>
2026-04-23 17:09:35 +0200

    Refactor main.jsx to use HashRouter and AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { AppProvider } from './context/AppContext'
-import MobileLayout from './MobileLayout'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import App from './App';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppProvider>
-      <MobileLayout />
-    </AppProvider>
+    <HashRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </HashRouter>
   </StrictMode>
-)
+);
